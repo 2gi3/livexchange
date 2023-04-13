d0b9a7a55bcccb200b74eb3f0c902e4dfcbcd397
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2023-04-13 22:51:00 +0100

    About Me page first draft

--- src/pages/aboutMe.tsx
+++ src/pages/aboutMe.tsx
@@ -1,5 +1,48 @@
 function aboutMe() {
-  return <div>About me</div>;
+  return (
+    <div>
+      <ul className="flex flex-wrap justify-evenly font-semibold gap-baseS p-base pb-11 max-w-xl mx-auto">
+        <li>Next.js</li>
+        <li>React</li>
+        <li>TypeScript</li>
+        <li>Javascript</li>
+        <li>D3.js</li>
+      </ul>
+      <ul className="flex flex-wrap justify-evenly font-semibold gap-baseS px-base pb-11 max-w-xl mx-auto">
+        <li>Tailwind</li>
+        <li>Bootstrap</li>
+        <li>SCSS</li>
+        <li>Styled Components</li>
+        <li>Figma</li>
+      </ul>
+      <ul className="flex flex-wrap justify-evenly font-semibold gap-baseS px-base pb-11 max-w-xl mx-auto">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>SQL</li>
+        <li>MongoDB</li>
+      </ul>
+      <ul className="flex flex-wrap justify-evenly font-semibold gap-baseS px-base pb-11 max-w-xl mx-auto">
+        <li>Jest</li>
+        <li>Cypress</li>
+        <li>CI/CD</li>
+      </ul>
+      <div>
+        <h3>Work experience</h3>
+      </div>
+      <div>
+        <h3>Education</h3>
+      </div>
+      <div className="flex flex-wrap justify-start px-base max-w-xl mx-auto">
+        <a
+          href="https://github.com/2gi3"
+          target="blank"
+          className="inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-gray-600 hover:text-gray-600 hover:bg-white  mt-11 "
+        >
+          My Github
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default aboutMe;
